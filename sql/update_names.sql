954149162b7d17edd14d8e981d6825e867fe2604
Koo Chia Wei <Koo_Chia_Wei@simtech.a-star.edu.sg>
2024-04-17 10:53:56 +0800

    sync to ecoinvent (#1)

--- sql/update_names.sql
+++ sql/update_names.sql
@@ -25,6 +25,69 @@ UPDATE ne_countries SET name = 'United States of America' where name = 'United S
 SELECT SingleCountry('Czech Republic');
 UPDATE ne_countries SET name = 'Czechia' where name = 'Czech Republic';
 
+SELECT SingleCountry('Brunei Darussalam');
+UPDATE ne_countries SET name = 'Brunei' where name = 'Brunei Darussalam';
+
+SELECT SingleCountry('Republic of Cabo Verde');
+UPDATE ne_countries SET name = 'Cape Verde' where name = 'Republic of Cabo Verde';
+
+SELECT SingleCountry('Kingdom of eSwatini');
+UPDATE ne_countries SET name = 'Eswatini' where name = 'Kingdom of eSwatini';
+
+SELECT SingleCountry('Falkland Islands / Malvinas');
+UPDATE ne_countries SET name = 'Falkland Islands' where name = 'Falkland Islands / Malvinas';
+
+SELECT SingleCountry('Faeroe Islands');
+UPDATE ne_countries SET name = 'Faroe Islands' where name = 'Faeroe Islands';
+
+-- SELECT SingleCountry('French Southern and Antarctic Lands');
+-- UPDATE ne_countries SET name = 'French Southern Territories' where name = 'French Southern and Antarctic Lands';
+
+SELECT SingleCountry('The Gambia');
+UPDATE ne_countries SET name = 'Gambia' where name = 'The Gambia';
+
+SELECT SingleCountry('Côte d''Ivoire');
+UPDATE ne_countries SET name = 'Ivory Coast' where name = 'Côte d''Ivoire';
+
+SELECT SingleCountry('Lao PDR');
+UPDATE ne_countries SET name = 'Laos' where name = 'Lao PDR';
+
+SELECT SingleCountry('South Georgia and the Islands');
+UPDATE ne_countries SET name = 'South Georgia and South Sandwich Islands' where name = 'South Georgia and the Islands';
+
+SELECT SingleCountry('Wallis and Futuna Islands');
+UPDATE ne_countries SET name = 'Wallis and Futuna' where name = 'Wallis and Futuna Islands';
+
+SELECT SingleCountry('Saint-Martin');
+UPDATE ne_countries SET name = 'Saint Martin' where name = 'Saint-Martin';
+
+SELECT SingleCountry('Timor-Leste');
+UPDATE ne_countries SET name = 'East Timor' where name = 'Timor-Leste';
+
+SELECT SingleCountry('Saint-Barthélemy');
+UPDATE ne_countries SET name = 'Saint Barthelemy' where name = 'Saint-Barthélemy';
+
+SELECT SingleCountry('São Tomé and Principe');
+UPDATE ne_countries SET name = 'Sao Tome and Principe' where name = 'São Tomé and Principe';
+
+SELECT SingleCountry('Guinea-Bissau');
+UPDATE ne_countries SET name = 'Guinea Bissau' where name = 'Guinea-Bissau';
+
+SELECT SingleCountry('Bahamas');
+UPDATE ne_countries SET name = 'The Bahamas' where name = 'Bahamas';
+
+SELECT SingleCountry('Hong Kong');
+UPDATE ne_countries SET name = 'Hong Kong S.A.R.' where name = 'Hong Kong';
+
+SELECT SingleCountry('Macao');
+UPDATE ne_countries SET name = 'Macao S.A.R.' where name = 'Macao';
+
+SELECT SingleCountry('Republic of Korea');
+UPDATE ne_countries SET name = 'South Korea' where name = 'Republic of Korea';
+
+SELECT SingleCountry('Dem. Rep. Korea');
+UPDATE ne_countries SET name = 'North Korea' where name = 'Dem. Rep. Korea';
+
 SELECT SingleCountry('Federated States of Micronesia');
 UPDATE ne_countries SET name = 'Micronesia, Federated States of' where name = 'Federated States of Micronesia';
 
@@ -34,8 +97,14 @@ UPDATE ne_countries SET name = 'Micronesia, Federated States of' where name = 'F
 SELECT SingleCountry('Åland Islands');
 UPDATE ne_countries SET name = 'Aland' where name = 'Åland Islands';
 
+SELECT SingleProvince('Heilongjiang');
 UPDATE ne_provinces SET name_local = '黑龙江省' WHERE name = 'Heilongjiang';
+
+SELECT SingleProvince('Qinghai');
 UPDATE ne_provinces SET name_local = '青海' WHERE name = 'Qinghai';
+
+SELECT SingleCountry('Russian Federation');
+UPDATE ne_countries SET name = 'Russia' WHERE name = 'Russian Federation';
 
 SELECT SingleCountry('Akrotiri');
 UPDATE ne_countries SET name = 'Akrotiri Sovereign Base Area' WHERE name = 'Akrotiri';
